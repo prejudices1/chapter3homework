853dffe69f494f5d7ddfa9f725014628c72177e1
dyx <1729251595@qq.com>
2020-05-23 21:45:31 +0800

    第三次作业

---
+++ app/src/main/java/com/example/chapter3/homework/ItemAdapter.java
@@ -0,0 +1,65 @@
+package com.example.chapter3.homework;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.numviewholder> {
+
+    private static final String TAG = "ItemAdapter";
+
+
+    private static int viewHolderCount;
+    Context context;
+
+    public ItemAdapter(Context context) {
+        this.context = context;
+        viewHolderCount = 0;
+    }
+
+    @NonNull
+    @Override
+    public numviewholder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        Context context = viewGroup.getContext();
+        int layoutIdForListItem = R.layout.item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+        numviewholder viewHolder = new numviewholder(view);
+
+        int num = viewHolderCount;
+        viewHolder.textview.setText("好友 " + num);
+
+        viewHolderCount++;
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull numviewholder numberViewHolder, int position) {
+        numberViewHolder.bind(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+
+    public class numviewholder extends RecyclerView.ViewHolder {
+
+        private final TextView textview;
+
+        public numviewholder(@NonNull View itemView) {
+            super(itemView);
+            textview = (TextView) itemView.findViewById(R.id.text_view);
+        }
+
+        public void bind(int position) {
+            ;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/chapter3/homework/PlaceholderFragment.java
@@ -0,0 +1,71 @@
+package com.example.chapter3.homework;
+
+
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class PlaceholderFragment extends Fragment {
+
+    private ItemAdapter itemAdapter;
+    private RecyclerView list;
+    private LottieAnimationView animationView;
+    private AnimatorSet set;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // TODO ex3-3: 修改 fragment_place holder，添加 loading控件和列表视图控件
+
+        View view = inflater.inflate(R.layout.fragment_placeholder, container, false);
+        animationView=view.findViewById(R.id.animation_view);
+        list = view.findViewById(R.id.list);
+        itemAdapter=new ItemAdapter(getActivity());
+
+        list.setLayoutManager(new LinearLayoutManager(getActivity(),LinearLayoutManager.VERTICAL,false));
+        list.setItemAnimator(new DefaultItemAnimator());
+        list.setAdapter(itemAdapter);
+        list.addItemDecoration(new DividerItemDecoration(getActivity(),DividerItemDecoration.VERTICAL));
+
+        list.setVisibility(View.GONE);
+
+        return view;//不能再返回inflater.inflate(R.layout.fragment_placeholder, container, false)
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        getView().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // 这里会在 5s 后执行
+                // TODO ex3-4：实现动画，将 lottie 控件淡出，列表数据淡入
+                ObjectAnimator alpha1 = ObjectAnimator.ofFloat(animationView,
+                        "alpha",1,0);
+                alpha1.setDuration(800);
+
+                ObjectAnimator alpha2 = ObjectAnimator.ofFloat(list,
+                        "alpha",0,1);
+                alpha2.setDuration(800);
+
+                list.setVisibility(View.VISIBLE);
+                set = new AnimatorSet();
+                set.playTogether(alpha1, alpha2);
+                set.start();
+            }
+        }, 5000);
+    }
+}
